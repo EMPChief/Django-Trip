d170bbcc13ba5b93fae4407b5fcd8ff3f70f1e30
empchief <bjornmagnekristensen1999@gmail.com>
2024-01-16 17:33:09 +0100

    hm

--- trips/urls.py
+++ trips/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import HomeView
+from .views import HomeView, trips_list
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('dashboard/', trips_list, name='trips-list'),
 ]
--- trips/views.py
+++ trips/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Trip, Note
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'trips/index.html'
+    
+    
+def trips_list(request):
+    trips = Trip.objects.all()
+    notes = Note.objects.all()
+    context = {
+        'trips': trips,
+        'notes': notes
+    }
+    return render(request, 'trips/trip_list.html', context)
