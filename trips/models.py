61309ad6ee76a6b7924f99c408a79e804a4c9aa8
empchief <bjornmagnekristensen1999@gmail.com>
2024-01-22 21:49:03 +0100

    Update models and base template

--- trips/models.py
+++ trips/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.core.exceptions import ValidationError
+import datetime  # import datetime if you want to set a default date
 
 class Trip(models.Model):
-    city = models.CharField(max_length=200, blank=True, null=True)
-    country = models.CharField(max_length=200, blank=True, null=True)
-    start_date = models.DateField(blank=True, null=True)
-    end_date = models.DateField(blank=True, null=True)
+    city = models.CharField(max_length=200, blank=True, null=True, default='Not Specified')
+    country = models.CharField(max_length=200, blank=True, null=True, default='Not Specified')
+    start_date = models.DateField(blank=True, null=True, default=datetime.date.today)
+    end_date = models.DateField(blank=True, null=True, default=datetime.date.today)
     owner = models.ForeignKey(User, related_name='trips', on_delete=models.CASCADE)
     
     def clean(self):
@@ -30,8 +31,8 @@ class Note(models.Model):
     )
     trip = models.ForeignKey(Trip, related_name='notes', on_delete=models.CASCADE)
     note = models.TextField(max_length=500, blank=True, null=True)
-    name = models.CharField(max_length=100, blank=True, null=True)
-    type = models.CharField(max_length=100, choices=EXCURSION, blank=True, null=True)
+    name = models.CharField(max_length=100, blank=True, null=True, default='General')
+    type = models.CharField(max_length=100, choices=EXCURSION, blank=True, null=True, default='Other')
     img = models.ImageField(upload_to='notes/', blank=True, null=True)
     rating = models.IntegerField(
         blank=True, 
@@ -39,9 +40,9 @@ class Note(models.Model):
         validators=[
             MaxValueValidator(5),
             MinValueValidator(1)
-        ]
+        ],
+        default=3
     )
 
     def __str__(self):
         return f'Note for {self.trip}: {self.note[:20]}...'
-
